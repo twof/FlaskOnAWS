c8a2bbb49617bee9dbcba65ae293eabd71c9a3aa
twof <fabiobean2@gmail.com>
2019-02-20 17:36:24 -0800

    fixes

--- manage.py
+++ manage.py
@@ -4,14 +4,12 @@ from flask_migrate import Migrate, MigrateCommand
 
 from main import app, db
 
-
-app.config.from_object(os.environ['APP_SETTINGS'])
+#app.config.from_object(os.environ['APP_SETTINGS'])
 
 migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
-
 
 if __name__ == '__main__':
     manager.run()
